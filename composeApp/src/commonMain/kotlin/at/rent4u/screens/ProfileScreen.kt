d309d3a223f94c45da3bf5f3dea9f5c081cf8ede
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2025-05-09 11:05:06 +0200

    add logout button on profile screen

--- composeApp/src/commonMain/kotlin/at/rent4u/screens/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/at/rent4u/screens/ProfileScreen.kt
@@ -2,12 +2,19 @@ package at.rent4u.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import at.rent4u.auth.UserAuth
 
 @Composable
 fun ProfileScreen(navController: NavController) {
@@ -15,10 +22,29 @@ fun ProfileScreen(navController: NavController) {
         bottomBar = { BottomNavBar(navController) }
     ) {
         Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            contentAlignment = Alignment.BottomCenter
         ) {
-            Text("Profile Screen")
+            Button(
+                onClick = {
+                    val userAuth = UserAuth()
+                    userAuth.logoutUser()
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(0) { inclusive = true }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .padding(bottom = 32.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.LightGray,
+                    contentColor = Color.Black
+                )
+            ) {
+                Text("Logout")
+            }
         }
     }
 }
